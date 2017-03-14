94b13a84c11c0732ec7729cd637345d1f8193e1d
Su Kim <su.w.kim@gmail.com>
2017-03-14 01:32:18 -0400

    edited out user and group

--- index.js
+++ index.js
@@ -16,14 +16,12 @@ function createError(errorMessage) {
 
 function getUsageHelp(commandName) {
     function createSample(target) {
-        return commandName + ' *' + target + '* I know what you did last summer';
+        return commandName + ' *' + target + '* [Your message]';
     }
 
     var text = 'Expected usage: \n' +
         commandName + ' help -- Displays help message.\n' +
-        createSample('@user') + ' -- Sends to the specified user.\n' +
         createSample('#channel') + ' -- Sends to the specified public channel.\n' +
-        createSample('group') + ' -- Sends to the specified private group.\n' +
         createSample(':here') + ' -- Sends to the current group/channel/DM where you type this command.';
 
     return text;
@@ -31,7 +29,7 @@ function getUsageHelp(commandName) {
 
 function getFullHelp(commandName) {
     var text =
-        'Allows to send anonymous messages to users, channels and groups.\n' +
+        'Allows to send anonymous messages to channels.\n' +
         'The most convenient and safe way is to open up a conversation with slackbot in Slack and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
         'Messages and authors are not stored, and the sources are available at <https://github.com/TargetProcess/slack-anonymous>.\n' +
         '\n' +
